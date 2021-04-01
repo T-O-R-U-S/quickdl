fb73ff70a8618ba7ed48cc45fb9c90c8c37b0e55
T-O-R-U-S <bageliq@protonmail.com>
2021-04-01 20:08:29 +0400

    Added error handling (for when the user inputs an invalid URL)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use reqwest::blocking::get;
-
+#[allow(unused_imports)]
 use std::env::args;
+#[allow(unused_imports)]
 use std::io::prelude::*;
+#[allow(unused_imports)]
 use std::fs::File;
-
+#[allow(unused_imports)]
 use console::style;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
+
     let data_to_dl:Vec<String> = args().collect();
     
     if data_to_dl.len() == 1 {
@@ -17,8 +19,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let data_string = format!("{}", data_to_dl[1]);
 
-    let data = get(&data_string)?
-        .text()?;
+    let data = match get(&data_string) {
+        Ok(file) => file,
+        Err(err) => {
+             println!("{}", style(&format!("Error: {}", err)).red());
+             return Ok(());
+        }
+    };
+    let data = match data.text() {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Failed to process data: {}", err);
+            return Ok(());
+        }
+    };
 
     if data_to_dl.len() < 3 {
 
@@ -47,6 +61,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("{}", style("Done! ✔").green());
-
     Ok(())
 }
